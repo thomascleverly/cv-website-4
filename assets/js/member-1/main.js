d542a32216554c81dd3e3cc13165d90046f79151
thomascleverly <99171103+thomascleverly@users.noreply.github.com>
2022-03-30 03:18:51 +0700

    Tuan Anh Code Testimonial

--- assets/js/member-1/main.js
+++ assets/js/member-1/main.js
@@ -1,6 +1,12 @@
-window.addEventListener('scroll',(event) => {
-    // document.querySelector('#header').style.height = '90px'
-    document.querySelector('#header').style.boxShadow = '5px 5px 15px #d1d9e6,-5px -5px 15px #fff';
+window.addEventListener('scroll', event => {
+    if (window.scrollY>200){
+        document.querySelector('#header').className = 'header-fixed';
+        document.querySelector('#introduce').style.paddingTop = '120px';
+    }
+    else{
+        document.querySelector('#header').className = '';
+        document.querySelector('#introduce').style.paddingTop = '0';
+    }
 });
 
 var menusInner = document.querySelectorAll('.menu-inner')
@@ -63,13 +69,41 @@ resumeMenus.forEach(item => {
     item.addEventListener('mouseout', event => {
         if (item.textContent != resumeMenuIsSelected)
             item.className = 'resume-menu'
-        // resumeMenus.forEach(element => {
-        //     if (element.textContent != item.textContent)
-        //         item.className = 'resume-menu'
-        // })
     })
 })
 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("tes-slider")
+  var y = document.getElementsByClassName("tes-radio-btn")
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    y[i].className = "tes-radio-btn"  
+  }
+  x[slideIndex-1].style.display = "block"; 
+  y[slideIndex-1].className = 'tes-radio-btn active' 
+}
+
+function btnShowDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("tes-slider")
+    var y = document.getElementsByClassName("tes-radio-btn")
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+      y[i].className = "tes-radio-btn"
+    }
+    x[n].style.display = "block";  
+    y[n].className = 'tes-radio-btn active'
+}
 
 
 
