6859dc2e38bd0ad3592bc9b721298a79d3b1ac5f
Ha Thanh Huyen <2051063560@e.tlu.edu.vn>
2022-03-31 01:48:49 +0700

    31/3 header

--- assets/js/member-2/main.js
+++ assets/js/member-2/main.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
         $('.resume-menu').removeClass("active");
       $(this).addClass("active");
     });
+    // $(".tabAwesome").click(function(){
+    //     $('.tabAwesome').removeClass("active"); // tương tự cái dưới
+    //   $(this).addClass("active");
+    //   console.log(this.id)
+      
+    //   $(`.listTab .row`).addClass("d-none"); 
+    //   $(`.${this.id}`).removeClass('d-none')
+    //     });
+    
   });
