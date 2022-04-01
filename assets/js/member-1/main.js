251f596557a12d4848b2bb1e2ce5257e20feac05
thomascleverly <99171103+thomascleverly@users.noreply.github.com>
2022-04-02 04:07:37 +0700

    Tuan Anh code hoan thien phan Resume va code 50% phan Clients web CV

--- assets/js/member-1/main.js
+++ assets/js/member-1/main.js
@@ -1,88 +1,101 @@
-window.addEventListener('scroll', event => {
-    if (window.scrollY>200){
-        document.querySelector('#header').className = 'header-fixed';
-        document.querySelector('#introduce').style.paddingTop = '120px';
-    }
-    else{
-        document.querySelector('#header').className = '';
-        document.querySelector('#introduce').style.paddingTop = '0';
-    }
-});
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if (this.scrollY>200){
+            $('#header').addClass('header-fixed')
+            $('#introduce').css('paddingTop','120px')
+        }
+        else{
+            $('#header').removeClass('header-fixed')
+            $('#introduce').css('paddingTop','0')
+        }
 
-var menusInner = document.querySelectorAll('.menu-inner')
-var pageIsSelecting = 'Home'
-menusInner.forEach(item => {
-    item.addEventListener('click', event => {
-        item.style.color = '#ff014f'
-        pageIsSelecting = item.textContent
-        menusInner.forEach(element => {
-            if (element.textContent != item.textContent)
-                element.style.color = '#3C3E31'
+        if (this.scrollY>3200){
+            $('.menu-inner').removeClass('active')
+            $('.menu-inner').each(function(){
+                if ($(this).text()=='Clients')
+                    $(this).addClass('active')
+            })
+        }
+        else if (this.scrollY>700){
+            $('.menu-inner').removeClass('active')
+            $('.menu-inner').each(function(){
+                if ($(this).text()=='Resume')
+                    $(this).addClass('active')
+            })
+        }
+        else{
+            $('.menu-inner').removeClass('active')
+            $('.menu-inner').each(function(){
+                if ($(this).text()=='Home')
+                    $(this).addClass('active')
+            })
+        }
+        // if (this.scrollY>1000){
+        //     $('.menu-inner').removeClass('active')
+        //     $('.menu-inner').each(function(){
+        //         if ($(this).text()=='Clients')
+        //             $(this).addClass('active')
+        //     })
+        // }
+    })
+
+    // Header Menu
+    let menuIsSelected = 'Home'
+    $('.menu-inner').click(function(){
+        $('.menu-inner').removeClass('active')
+        $(this).addClass('active')
+        menuIsSelected = $(this).text()
+    })
+
+    $('.menu-inner').hover(function(){
+        let menu = $(this).text()
+        $('.menu-inner').each(function(){
+            if ($(this).text()!=menuIsSelected && $(this).text()!=menu)
+                $(this).css('opacity','0.6')
         })
+        }, function(){
+            $('.menu-inner').css('opacity','1')
+        }
+    )
+
+    // Resume Menu
+    let resumeMenuIsSelected = 'Education'
+    $('.resume-list').hide()
+    $(`.${resumeMenuIsSelected.toLowerCase()}`).fadeIn()
+    $('.resume-menu').click(function(){
+        $('.resume-menu').removeClass('active')
+        $(this).addClass('active')
+        resumeMenuIsSelected = $(this).text()
+        console.log(resumeMenuIsSelected)
+        $('.resume-list').hide()
+        if (resumeMenuIsSelected=='Professional Skills')
+            $('.proskills').fadeIn()
+        else
+            $(`.${resumeMenuIsSelected.toLowerCase()}`).fadeIn()
     })
+
+    $('.resume-menu').hover(function(){
+        $(this).addClass('active')
+        }, function(){
+            $('.resume-menu').each(function(){
+                if ($(this).text()!=resumeMenuIsSelected)
+                    $(this).removeClass('active')
+            })
+        }
+    )
+
+
 })
 
-menusInner.forEach(item => {
-    item.addEventListener('mouseover', event => {
-        item.style.opacity = '1'
-        menusInner.forEach(element => {
-            if (element.textContent != item.textContent && element.textContent != pageIsSelecting)
-                element.style.opacity = '0.6'
-        })
-    })
-})
-
-menusInner.forEach(item => {
-    item.addEventListener('mouseout', event => {
-        item.style.opacity = '1'
-        menusInner.forEach(element => {
-            element.style.opacity = '1'
-        })
-    })
-})
-
-
-resumeMenuIsSelected = 'Education'
-var resumeMenus = document.querySelectorAll('.resume-menu')
-resumeMenus.forEach(item => {
-    item.addEventListener('click', event => {
-        item.className = 'resume-menu active'
-        resumeMenuIsSelected = item.textContent
-        resumeMenus.forEach(element => {
-            if (element.textContent != item.textContent)
-                element.className = 'resume-menu'
-        })
-    })
-})
-
-resumeMenus.forEach(item => {
-    item.addEventListener('mouseover', event => {
-        item.className = 'resume-menu active'
-        resumeMenus.forEach(element => {
-            if (element.textContent != item.textContent && element.textContent != resumeMenuIsSelected)
-                element.className = 'resume-menu'
-        })
-    })
-})
-
-resumeMenus.forEach(item => {
-    item.addEventListener('mouseout', event => {
-        if (item.textContent != resumeMenuIsSelected)
-            item.className = 'resume-menu'
-    })
-})
-
-var slideIndex = 1;
+let slideIndex = 1;
 showDivs(slideIndex);
-
 function plusDivs(n) {
   showDivs(slideIndex += n);
 }
-
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("tes-slider")
-  var y = document.getElementsByClassName("tes-radio-btn")
+  var x = $(".tes-slider")
+  var y = $(".tes-radio-btn")
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
@@ -95,8 +108,8 @@ function showDivs(n) {
 
 function btnShowDivs(n) {
     var i;
-    var x = document.getElementsByClassName("tes-slider")
-    var y = document.getElementsByClassName("tes-radio-btn")
+    var x = $(".tes-slider")
+    var y = $(".tes-radio-btn")
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";  
       y[i].className = "tes-radio-btn"
