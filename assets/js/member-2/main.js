8f7532491a72b9a93443ffa73ef61e18c1917db5
Ha Thanh Huyen <2051063560@e.tlu.edu.vn>
2022-04-02 20:15:19 +0700

    fix bugs and pricing and contact

--- assets/js/member-2/main.js
+++ assets/js/member-2/main.js
@@ -1,31 +1,66 @@
- $(document).ready(function(){
-     $(".resume-menu").click(function(){
-         $('.resume-menu').removeClass("active");
-       $(this).addClass("active");
-     });
-    // $(".tabAwesome").click(function(){
-    //     $('.tabAwesome').removeClass("active"); // tương tự cái dưới
-    //   $(this).addClass("active");
-    //   console.log(this.id)
-      
-    //   $(`.listTab .row`).addClass("d-none"); 
-    //   $(`.${this.id}`).removeClass('d-none')
-    //     });
-    
-
-  // $('.premium').hide();
-  // $('.standard').hide();
-  // $('.basic').fadeIn();
-  // $('.pricinglist').on('click',function(){
-  //     $(this).siblings().removeClass('active');
-  //     $(this).addClass('active');
-
-  //     // Show 
-  //     let id_tab = $(this).children('a').attr('href');
-  //     $('.premium').hide();
-  //     $('.standard').hide();
-  //     $('.basic').hide();
-  //     $(id_tab).fadeIn();
-  //     return false;
-  // })
+ window.addEventListener('scroll', event => {
+    if (window.scrollY>200){
+        document.querySelector('#header').className = 'header-fixed';
+        document.querySelector('#introduce').style.paddingTop = '120px';
+    }
+    else{
+        document.querySelector('#header').className = '';
+        document.querySelector('#introduce').style.paddingTop = '0';
+    }
 });
+
+var menusInner = document.querySelectorAll('.menu-inner')
+var pageIsSelecting = 'Home'
+menusInner.forEach(item => {
+    item.addEventListener('click', event => {
+        item.style.color = '#ff014f'
+        pageIsSelecting = item.textContent
+        menusInner.forEach(element => {
+            if (element.textContent != item.textContent)
+                element.style.color = '#3C3E31'
+        })
+    })
+})
+
+
+$(document).ready(function(){
+
+
+  $('.item').on('click',function(){
+      $(this).siblings().removeClass('active-item');
+      $(this).addClass('active-item');
+  })
+
+  $('.skills').hide();
+  $('.row-education').fadeIn();
+  $('.resumelist').on('click',function(){
+      $(this).siblings().removeClass('active');
+      $(this).addClass('active');
+
+      // Show 
+      let id_tab = $(this).children('a').attr('href');
+      $('.row-education').hide();
+      $('.skills').hide();
+      $(id_tab).fadeIn();
+      return false;
+  })
+
+
+
+//Pricing
+  $('.premium').hide();
+  $('.standard').hide();
+  $('.basic').fadeIn();
+  $('.pricinglist').on('click',function(){
+      $(this).siblings().removeClass('active');
+      $(this).addClass('active');
+
+      // Show 
+      let id_tab = $(this).children('a').attr('href');
+      $('.premium').hide();
+      $('.standard').hide();
+      $('.basic').hide();
+      $(id_tab).fadeIn();
+      return false;
+  })
+})
