29f98dbcf51dbfddf743918ad2141690d9b30fa5
Ha Thanh Huyen <2051063560@e.tlu.edu.vn>
2022-04-04 23:43:55 +0700

    Fix bugs and done contact

--- assets/js/member-2/main.js
+++ assets/js/member-2/main.js
@@ -1,66 +1,65 @@
- window.addEventListener('scroll', event => {
-    if (window.scrollY>200){
-        document.querySelector('#header').className = 'header-fixed';
-        document.querySelector('#introduce').style.paddingTop = '120px';
-    }
-    else{
-        document.querySelector('#header').className = '';
-        document.querySelector('#introduce').style.paddingTop = '0';
-    }
+window.addEventListener("scroll", (event) => {
+  if (window.scrollY > 200) {
+    document.querySelector("#header").className = "header-fixed";
+    document.querySelector("#introduce").style.paddingTop = "120px";
+  } else {
+    document.querySelector("#header").className = "";
+    document.querySelector("#introduce").style.paddingTop = "0";
+  }
 });
 
-var menusInner = document.querySelectorAll('.menu-inner')
-var pageIsSelecting = 'Home'
-menusInner.forEach(item => {
-    item.addEventListener('click', event => {
-        item.style.color = '#ff014f'
-        pageIsSelecting = item.textContent
-        menusInner.forEach(element => {
-            if (element.textContent != item.textContent)
-                element.style.color = '#3C3E31'
-        })
-    })
-})
+var menusInner = document.querySelectorAll(".menu-inner");
+var pageIsSelecting = "Home";
+menusInner.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.style.color = "#ff014f";
+    pageIsSelecting = item.textContent;
+    menusInner.forEach((element) => {
+      if (element.textContent != item.textContent)
+        element.style.color = "#3C3E31";
+    });
+  });
+});
 
+$(document).ready(function () {
+  $(".item").on("click", function () {
+    $(this).siblings().removeClass("active-item");
+    $(this).addClass("active-item");
+  });
 
-$(document).ready(function(){
+  $(".skills").hide();
+  $(".row-education").fadeIn();
+  $(".resumelist").on("click", function () {
+    $(this).siblings().removeClass("active");
+    $(this).addClass("active");
 
+    // Show
+    let id_tab = $(this).children("a").attr("href");
+    $(".row-education").hide();
+    $(".skills").hide();
+    $(id_tab).fadeIn();
+    return false;
+  });
+//   Clients
+  $(".nav-item").on("click", function () {
+    $(this).siblings().removeClass("active");
+    $(this).addClass("active");
+  });
 
-  $('.item').on('click',function(){
-      $(this).siblings().removeClass('active-item');
-      $(this).addClass('active-item');
-  })
+  //Pricing
+  $(".premium").hide();
+  $(".standard").hide();
+  $(".basic").fadeIn();
+  $(".pricinglist").on("click", function () {
+    $(this).siblings().removeClass("active");
+    $(this).addClass("active");
 
-  $('.skills').hide();
-  $('.row-education').fadeIn();
-  $('.resumelist').on('click',function(){
-      $(this).siblings().removeClass('active');
-      $(this).addClass('active');
-
-      // Show 
-      let id_tab = $(this).children('a').attr('href');
-      $('.row-education').hide();
-      $('.skills').hide();
-      $(id_tab).fadeIn();
-      return false;
-  })
-
-
-
-//Pricing
-  $('.premium').hide();
-  $('.standard').hide();
-  $('.basic').fadeIn();
-  $('.pricinglist').on('click',function(){
-      $(this).siblings().removeClass('active');
-      $(this).addClass('active');
-
-      // Show 
-      let id_tab = $(this).children('a').attr('href');
-      $('.premium').hide();
-      $('.standard').hide();
-      $('.basic').hide();
-      $(id_tab).fadeIn();
-      return false;
-  })
-})
+    // Show
+    let id_tab = $(this).children("a").attr("href");
+    $(".premium").hide();
+    $(".standard").hide();
+    $(".basic").hide();
+    $(id_tab).fadeIn();
+    return false;
+  });
+});
