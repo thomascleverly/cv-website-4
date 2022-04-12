81931499ce91dc4ae613320821fe1b168f9acc74
Ha Thanh Huyen <2051063560@e.tlu.edu.vn>
2022-04-12 20:59:53 +0700

    Fix nut admin va fix mot so loi

--- assets/js/member-2/main.js
+++ assets/js/member-2/main.js
@@ -63,3 +63,87 @@ $(document).ready(function () {
     return false;
   });
 });
+$(window).scroll(function(){
+  if (this.scrollY>200){
+      $('#header').addClass('header-fixed')
+      $('#introduce').css('paddingTop','120px')
+  }
+  else{
+      $('#header').removeClass('header-fixed')
+      $('#introduce').css('paddingTop','0')
+  }
+})
+
+//Mobile Menu
+$('.mobile-menu').hide()
+// $('.btn-bar').hide()
+$('.model').hide()
+$('.btn-bar').click(function(){
+  let id_menu = $(this).children('a').attr('href');
+  $('.mobile-menu').fadeIn()
+  $('.model').fadeIn()
+  $(id_menu).fadeIn()
+  return false
+})
+$('.btn-close').click(function(){
+  let id_btn = $(this).children('a').attr('href');
+  $('.mobile-menu').hide()
+  $('.model').hide()
+  $(id_btn).fadeIn()
+})
+
+$('.menu-inner').click(function(){
+  $('.menu-inner').removeClass('active')
+  $(this).addClass('active')
+  menuIsSelected = $(this).text()
+})
+
+$('.menu-inner').hover(function(){
+  let menu = $(this).text()
+  $('.menu-inner').each(function(){
+      if ($(this).text()!=menuIsSelected && $(this).text()!=menu)
+          $(this).css('opacity','0.4')
+  })
+  }, function(){
+      $('.menu-inner').css('opacity','0.8')
+  }
+)
+
+
+
+$('.skils').hide();
+$('.row-education').fadeIn();
+$('.resumelist').on('click',function(){
+  $(this).siblings().removeClass('active');
+  $(this).addClass('active');
+
+  // Show 
+  let id_tab = $(this).children('a').attr('href');
+  $('.row-education').hide();
+  $('.skils').hide();
+  $(id_tab).fadeIn();
+  return false;
+})
+
+  $('.nav-client').on('click',function(){
+  $(this).siblings().removeClass('active');
+  $(this).addClass('active');
+})
+
+
+
+$('.premium').hide();
+$('.standard').hide();
+$('.basic').fadeIn();
+$('.pricinglist').on('click',function(){
+  $(this).siblings().removeClass('active');
+  $(this).addClass('active');
+
+  // Show 
+  let id_tab = $(this).children('a').attr('href');
+  $('.premium').hide();
+  $('.standard').hide();
+  $('.basic').hide();
+  $(id_tab).fadeIn();
+  return false;
+})
